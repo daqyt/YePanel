631332fc4925e60c6bd9b48db58bef25816f50f8
1 <1>
2025-03-07 16:16:27 +0800

    1

---
+++ cs.js
@@ -0,0 +1,20 @@
+a = fetch("https://collector.github.com/github/collect", {
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "zh-CN,zh;q=0.9",
+    "content-type": "text/plain;charset=UTF-8",
+    "priority": "u=4, i",
+    "sec-ch-ua": "\"Not(A:Brand\";v=\"99\", \"Google Chrome\";v=\"133\", \"Chromium\";v=\"133\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "no-cors",
+    "sec-fetch-site": "cross-site",
+    "sec-fetch-storage-access": "active",
+    "Referer": "/https://github.com/Xhofe/alist/releases",
+    "Referrer-Policy": "no-referrer-when-downgrade"
+  },
+  "body": "{\"client_id\":\"1839673715.1741048767\",\"events\":[{\"page\":\"/https://github.com/Xhofe/alist/releases\",\"title\":\"Releases · AlistGo/alist\",\"context\":{\"user_id\":\"184931308\",\"user_login\":\"AlistGo\",\"repository_id\":\"323965659\",\"repository_nwo\":\"AlistGo/alist\",\"repository_public\":\"true\",\"repository_is_fork\":\"false\",\"repository_network_root_id\":\"323965659\",\"repository_network_root_nwo\":\"AlistGo/alist\",\"referrer\":\"\",\"request_id\":\"7624:16E9EF:533DF1D:563BC28:67C6566F\",\"visitor_id\":\"7901338442976873407\",\"region_edge\":\"sea\",\"region_render\":\"sea\",\"staff\":\"false\",\"react\":\"false\",\"reactPartials\":\"[\\\"keyboard-shortcuts-dialog\\\"]\",\"featureFlags\":\"[\\\"a11y_quote_reply_fix\\\",\\\"contentful_lp_optimize_image\\\",\\\"contentful_lp_hero_video_cover_image\\\",\\\"contentful_response_validation\\\",\\\"copilot_immersive_file_preview\\\",\\\"copilot_new_references_ui\\\",\\\"copilot_chat_repo_custom_instructions_preview\\\",\\\"copilot_chat_immersive_subthreading\\\",\\\"copilot_chat_retry_on_error\\\",\\\"copilot_chat_persist_submitted_input\\\",\\\"copilot_conversational_ux_history_refs\\\",\\\"copilot_chat_shared_chat_input\\\",\\\"copilot_editor_upsells\\\",\\\"copilot_implicit_context\\\",\\\"copilot_no_floating_button\\\",\\\"copilot_smell_icebreaker_ux\\\",\\\"copilot_spaces_multi_file_picker\\\",\\\"copilot_read_shared_conversation\\\",\\\"dotcom_chat_client_side_skills\\\",\\\"experimentation_azure_variant_endpoint\\\",\\\"failbot_handle_non_errors\\\",\\\"geojson_azure_maps\\\",\\\"ghost_pilot_confidence_truncation_25\\\",\\\"ghost_pilot_confidence_truncation_40\\\",\\\"github_models_o3_mini_streaming\\\",\\\"hovercard_accessibility\\\",\\\"insert_before_patch\\\",\\\"issues_react_remove_placeholders\\\",\\\"issues_react_blur_item_picker_on_close\\\",\\\"issues_react_include_bots_in_pickers\\\",\\\"marketing_pages_search_explore_provider\\\",\\\"remove_child_patch\\\",\\\"sample_network_conn_type\\\",\\\"swp_enterprise_contact_form\\\",\\\"site_copilot_extensions_ga\\\",\\\"site_copilot_extensions_hero\\\",\\\"site_copilot_plans_2_27\\\",\\\"site_copilot_vscode_link_update\\\",\\\"site_proxima_australia_update\\\",\\\"issues_react_create_milestone\\\",\\\"issues_react_cache_fix_workaround\\\",\\\"lifecycle_label_name_updates\\\"]\",\"ssr\":\"false\",\"controller\":\"\\\"releases\\\"\",\"action\":\"\\\"index\\\"\",\"routePattern\":\"\\\"/:user_id/:repository/releases(.:format)\\\"\",\"cpu\":\"\\\"xlg\\\"\",\"fid\":\"{\\\"name\\\":\\\"FID\\\",\\\"value\\\":2,\\\"element\\\":\\\"div.Box>div.Box-footer>div.mb-3>details-toggle>details>summary>span.f3.text-bold.d-inline.mr-3\\\"}\"},\"type\":\"web-vital\"}],\"request_context\":{\"referrer\":\"\",\"user_agent\":\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36\",\"screen_resolution\":\"1920x1080\",\"browser_resolution\":\"1365x919\",\"browser_languages\":\"zh-CN,zh\",\"pixel_ratio\":1,\"timestamp\":1741051510996,\"tz_seconds\":28800}}",
+  "method": "POST"
+});
+a
